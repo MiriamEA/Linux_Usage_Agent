212b2354fdd9453e082f6fc4c6d1dc0c4bc94442
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-06-24 22:04:28 +0000

    solutions.sql finished

--- solutions.sql
+++ solutions.sql
@@ -11,15 +11,15 @@ ORDER BY	cpu_number, total_mem DESC;
 SELECT	id,
 	hostname,
 	total_memory,
-	ROUND(used_memory_percentage, 1)
+	ROUND((total_memory - (1000*avg_free_mem))/total_memory, 1)
 FROM   (SELECT 		info.id,
 			info.hostname,
 			info.total_mem AS total_memory,
-			AVG(((info.total_mem / 1000) - use.memory_free)/(info.total_mem/1000))
+			AVG(use.memory_free)
 				OVER(	PARTITION BY info.id
 					ORDER BY use.timestamp
 					ROWS BETWEEN 4 PRECEDING AND CURRENT ROW)
-				AS used_memory_percentage
+				AS avg_free_mem
 	FROM		host_info info
 	INNER JOIN	host_usage use
 	ON		info.id = use.host_id
